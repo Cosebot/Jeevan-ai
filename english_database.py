bff49421aea7a8913e461dfb29a3ed62410a757b
Cosebot <jabirafarhath72@gmail.com>
2025-10-15 11:47:59 +0530

    Delete english_database.py

--- english_database.py
+++
@@ -1,7 +0,0 @@
-responses = {
-    "hello": ["Hello!", "Hi there!", "Hey! How can I help you today?"],
-    "how are you": ["I'm just a bot, but I'm doing great! How about you?", "I'm fine, thank you!"],
-    "help": ["Sure! What do you need help with?", "I'm here to assist you."],
-    "bye": ["Goodbye! Have a great day!", "See you later!", "Take care!"],
-    "thank you": ["You're welcome!", "No problem at all!", "Happy to help!"]
-}
